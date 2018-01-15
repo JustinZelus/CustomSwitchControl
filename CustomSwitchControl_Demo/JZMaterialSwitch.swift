e67b27f68fdae1b0f0a962e5d9d9e4d3990e8651
JZ <justin.qian.v@gmail.com>
2018-01-15 18:58:53 +0800

    add TouchUpinSide & TouchUpOutSide

--- CustomSwitchControl_Demo/JZMaterialSwitch.swift
+++ CustomSwitchControl_Demo/JZMaterialSwitch.swift
@@ -27,7 +27,9 @@ class JZMaterialSwitch : UIControl {
     weak var delegate: JZMaterialSwitchDelegate? = nil
     
     
-    
+    //MARK:  Colour
+    /** An UIColor property to represent the fill colour of the ripple only when ripple effect is enabled */
+    var rippleFillColor: UIColor = .gray
     
     //MARK: UI components
     /** An UIButton object that represent current state(ON/OFF) */
@@ -45,7 +47,8 @@ class JZMaterialSwitch : UIControl {
     var isRippleEnabled: Bool = false;
     
     fileprivate var thumbOffPosition: CGFloat!
-    
+    fileprivate var testLayer: CALayer!
+    fileprivate var rippleLayer: CAShapeLayer!
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -80,24 +83,18 @@ class JZMaterialSwitch : UIControl {
             self.thumbSize = 18.0
             break
         }
-            //test//
+        
+        
+        
+        self.isRippleEnabled = true
             //        self.isEnabled = false;
         
-        /*
-        print("frame.x : " + frame.origin.x.description)
-        print("frame.y : " + frame.origin.y.description)
-        print("before -- trackFrame.x : " + trackFrame.origin.x.description)
-        print("before -- trackFrame.y : " + trackFrame.origin.y.description)
-        */
+      
         trackFrame.size.height = self.trackThickness
         trackFrame.size.width = frame.size.width
         trackFrame.origin.x = 0.0
         trackFrame.origin.y = (frame.size.height - trackFrame.size.height) / 2
-        
-        /*
-        print("after -- trackFrame.x : " + trackFrame.origin.x.description)
-        print("after -- trackFrame.y : " + trackFrame.origin.y.description)
-        */
+      
         
         thumbFrame.size.height = self.thumbSize
         thumbFrame.size.width = thumbFrame.size.height
@@ -117,24 +114,75 @@ class JZMaterialSwitch : UIControl {
         
         self.switchThumb.layer.shadowColor = UIColor.purple.cgColor
         self.switchThumb.layer.shadowRadius = 2.0
-        
+
         self.addSubview(self.switchThumb)
         
         //Add events for user action
         self.switchThumb.addTarget(self, action: #selector(self.onTouchDown(btn:withEvent:)), for: UIControlEvents.touchDown)
+        self.switchThumb.addTarget(self, action: #selector(onTouchUpOutsideOrCanceled(btn:withEvent:)), for: UIControlEvents.touchUpOutside)
+    }
+    
+    //why不會觸發?
+    @objc func onTouchUpOutsideOrCanceled(btn: UIButton, withEvent event: UIEvent) {
+        print("Touch released at outside")
+        if let touch = event.touches(for: btn)?.first {
+            
+            let prevPos = touch.previousLocation(in: btn)
+            let pos = touch.location(in: btn)
+        }
     }
     
     //MARk: - Event Actions
     @objc func onTouchDown(btn: UIButton, withEvent event: UIEvent)
     {
-        // print("touchDown called")
+//         print("touchDown called")
         if self.isRippleEnabled {
             self.initializeRipple();
+            
+            // Animate for apperaing ripple circle when tap and hold the switch thumb
+             CATransaction.begin()
+            
+             let scaleAnimation = CABasicAnimation(keyPath: "transform.scale")
+             scaleAnimation.fromValue = 0.0
+             scaleAnimation.toValue = 1.0
+            
+             let alphaAnimation = CABasicAnimation(keyPath: "opacity")
+             alphaAnimation.fromValue = 0
+             alphaAnimation.toValue = 0.2
+            
+             // Do above animations at the same time with proper duration
+             let animation = CAAnimationGroup()
+             animation.animations = [scaleAnimation, alphaAnimation]
+             animation.duration = 0.4
+             animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut)
+            
+             rippleLayer.add(animation, forKey: nil)
+             CATransaction.commit()
         }
     }
     
     // Initialize and appear ripple effect
     func initializeRipple() {
+        // Ripple size is twice as large as switch thumb
         
+        let rippleScale : CGFloat = 2
+        var rippleFrame = CGRect.zero
+        rippleFrame.origin.x = -self.switchThumb.frame.size.width / (rippleScale * 2)
+        rippleFrame.origin.y = -self.switchThumb.frame.size.height / (rippleScale * 2)
+        rippleFrame.size.height = self.switchThumb.frame.size.height * rippleScale
+        rippleFrame.size.width = rippleFrame.size.height
+        
+        let path = UIBezierPath.init(roundedRect: rippleFrame, cornerRadius: self.switchThumb.layer.cornerRadius * 2)
+        
+        // Set ripple layer attributes
+        rippleLayer = CAShapeLayer()
+        rippleLayer.path = path.cgPath
+        rippleLayer.frame = rippleFrame
+        rippleLayer.opacity = 0.2
+        rippleLayer.strokeColor = UIColor.clear.cgColor
+        rippleLayer.fillColor = self.rippleFillColor.cgColor
+        rippleLayer.lineWidth = 0
+        
+        self.switchThumb.layer.insertSublayer(rippleLayer, below: self.switchThumb.layer)
     }
 }
